9fecc177956a42b3396157cc4fc7b957d1af7494
ddas4548 <ddas4548@gmail.com>
2025-08-29 11:14:26 +0530

    add repository  in submission service

---
+++ SubmissionService/src/repository/submission.repository.ts
@@ -0,0 +1,39 @@
+import { ISubmission, ISubmissionData, Submission, SubmissionStatus } from "../model/submission.model";
+
+
+export interface ISubmissionRepository {
+    create(submissionData: Partial<ISubmission>): Promise<ISubmission>;
+    findById(id: string): Promise<ISubmission | null>;
+    findByProblemId(problemId: string): Promise<ISubmission[]>;
+    deleteById(id: string): Promise<boolean>;
+    updateStatus(id: string, status: SubmissionStatus, submissionData: ISubmissionData): Promise<ISubmission | null>;
+}
+
+export class SubmissionRepository implements ISubmissionRepository {
+
+    async create(submissionData: Partial<ISubmission>): Promise<ISubmission> {
+        const newSubmission = await Submission.create(submissionData);
+        console.log("submission created successfully");
+        return newSubmission;
+    }
+
+    async findById(id: string): Promise<ISubmission | null> {
+        const submission =  await Submission.findById(id);
+        return submission;
+    }
+
+    async findByProblemId(problemId: string): Promise<ISubmission[]> {
+        const submissions = await Submission.find({problemId});
+        return submissions;
+    }
+
+    async deleteById(id: string): Promise<boolean> {
+        const result = await Submission.findByIdAndDelete(id);
+        return result !== null;
+    }
+
+    async updateStatus(id: string, status: SubmissionStatus, submissionData: ISubmissionData): Promise<ISubmission | null> {
+        const submission = await Submission.findByIdAndUpdate(id, {status, submissionData}, {new : true});
+        return submission;
+    }
+}
