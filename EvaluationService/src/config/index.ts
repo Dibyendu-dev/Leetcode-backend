ba257c27d8d617664f88c3feeaf949f08c150b67
ddas4548 <ddas4548@gmail.com>
2025-08-30 10:53:11 +0530

    config layer set up in evaluation service

--- EvaluationService/src/config/index.ts
+++ EvaluationService/src/config/index.ts
@@ -2,7 +2,11 @@
 import dotenv from 'dotenv';
 
 type ServerConfig = {
-    PORT: number
+    PORT: number,
+    REDIS_PORT:number,
+    REDIS_HOST:string,
+    PROBLEM_SERVICE:string,
+    SUBMISSION_SERVICE: string,
 }
 
 function loadEnv() {
@@ -13,5 +17,9 @@ function loadEnv() {
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+    PORT: Number(process.env.PORT) || 3001,
+    REDIS_PORT: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) :6379,
+    REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+    PROBLEM_SERVICE:process.env.PROBLEM_SERVICE || "http://localhost:3000/api/v1",
+    SUBMISSION_SERVICE:process.env.SUBMISSION_SERVICE || "http://localhost:3002/api/v1",
 };
