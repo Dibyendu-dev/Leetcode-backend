9a809cc405e5e1e7f94d30a5ba8bdcd613f41288
ddas4548 <ddas4548@gmail.com>
2025-09-02 11:23:36 +0530

    implement interface for evaluation service

---
+++ EvaluationService/src/interface/evaluation.interface.ts
@@ -0,0 +1,28 @@
+export interface Problem {
+    id: string;
+    title: string;
+    description: string;
+    difficulty: string;
+    editorial?: string;
+    testcases: TestCase[];
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface TestCase {
+    _id: string;
+    input: string;
+    output: string;
+}
+
+export interface EvaluationJob {
+    submissionId: string;
+    code: string;
+    language: "python" | "cpp";
+    problem: Problem;
+}
+
+export interface EvaluationResult {
+    status: string;
+    output: string | undefined;
+}
