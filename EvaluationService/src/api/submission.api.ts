c8eda81bbf40b03ce0ea93ceed66896061dba5ff
ddas4548 <ddas4548@gmail.com>
2025-09-02 16:27:30 +0530

    update submission from  evaluation service

---
+++ EvaluationService/src/api/submission.api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { serverConfig } from "../config";
+import logger from "../config/logger.config";
+import { InternalServerError } from "../utils/errors/app.error";
+
+
+export async function updateSubmission(submissionId: string, status:string, output: Record<string, string>) {
+
+   try {
+    const url = `${serverConfig.SUBMISSION_SERVICE}/submission/${submissionId}/status`;
+    logger.info("getting submission id", { url });
+
+    const response = await axios.patch(url, {
+        status,
+        submissionData: output
+    });
+
+    if (response.status !== 200) {
+      throw new InternalServerError("failed to update submission");
+    }
+
+    console.log("submission updated successfully", response.data);
+    return;
+   
+  } catch (error) {
+    logger.error(`failed to  update submission,${error}`);
+    return null;
+  }
+}
